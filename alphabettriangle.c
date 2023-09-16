b62f14e0097b0da2f6371126ad93680d1504e4ab
EgxAbhi <144133476+EgxAbhi@users.noreply.github.com>
2023-09-16 12:33:31 +0530

    Update alphabettriangle.c

--- alphabettriangle.c
+++ alphabettriangle.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    for(int i=1;i<=4;i++){
+        int a=1;
+        for(int j=1;j<=i;j++){
+            int d=a+64;
+            char ch=(char)d;
+            printf("%c",ch);
+            a++;
+            
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
